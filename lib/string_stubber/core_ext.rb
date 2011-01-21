8808b9fcc5d001d17a0f531dd94f39013684de72
Scott Gonyea <me@sgonyea.com>
2011-01-20 23:45:15 -0800

    Tests => Bugfixes, and Docs, for CoreExt

--- lib/string_stubber/core_ext.rb
+++ lib/string_stubber/core_ext.rb
@@ -2,10 +2,14 @@ module StringStubber
   module CoreExt
     include StringStubber::Base
 
+    # @return [StringScanner]
     def scanner
       @scanner ||= StringScanner.new(self)
     end
 
+    #   @param [Fixnum]
+    #   @param [true, false]
+    #   @return [String]
     def stub_words(max_words, save_pos=false)
       scanner.reset unless save_pos
 
@@ -18,53 +22,84 @@ module StringStubber
     alias :stub   :stub_words
     alias :words  :stub_words
 
-    def stub_at_most(max_chars, save_pos=true)
+    #   @param [Fixnum]
+    #   @param [true, false]
+    #   @return [String]
+    def stub_at_most(max_chars, save_pos=false)
       scanner.reset unless save_pos
 
-      if max_chars < 0
-        rev_stub_at_most(max_chars.abs, save_pos)
-      else
-        scan_text(scanner, max_chars)
-      end
+      str = if max_chars < 0
+              rev_stub_at_most(max_chars.abs, save_pos)
+            else
+              scan_text(scanner, max_chars)
+            end
+
+      scanner.unscan if str.empty? && scanner.pos > max_chars
+
+      return str
     end
     alias :stub_text  :stub_at_most
     alias :stub_chars :stub_at_most
     alias :stub_pos   :stub_at_most
     alias :stub_to    :stub_at_most
 
-    def stub_at_least(min_chars, save_pos=true)
+    #   @param [Fixnum]
+    #   @param [true, false]
+    #   @return [String]
+    def stub_at_least(min_chars, save_pos=false)
       scanner.reset unless save_pos
 
       if min_chars < 0
-        rev_stub_at_least(min_chars.abs, save_pos)
-      else
-        scan_text(scanner, min_chars) << scan_word(scanner).to_s
+        return rev_stub_at_least(min_chars.abs, save_pos)
       end
+
+      start = scanner.pos
+      str   = scan_text(scanner, min_chars)
+
+      if((scanner.pos - start) < min_chars)
+        str << scan_word(scanner).to_s
+      end
+
+      str
     end
     alias :stub_thru    :stub_at_least
     alias :stub_through :stub_at_least
 
-
   private
+    #   @param [Fixnum]
+    #   @param [true, false]
+    #   @return [String]
     def rev_stub_words(max_words, save_pos=false)
       str = self.reverse
       str.scanner.pos = 0 - scanner.pos if save_pos
 
-      return str.stub_words(max_words, save_pos).reverse!
+      stub = str.stub_words(max_words, save_pos).reverse!
+      scanner.pos = 0 - str.scanner.pos
+      return stub
     end
 
-    def rev_stub_at_most(max_chars, save_pos=true)
+    #   @param [Fixnum]
+    #   @param [true, false]
+    #   @return [String]
+    def rev_stub_at_most(max_chars, save_pos=false)
       str = self.reverse
       str.scanner.pos = 0 - scanner.pos if save_pos
 
-      return str.stub_at_most(max_chars, save_pos).reverse!
+      stub = str.stub_at_most(max_chars, save_pos).reverse!
+      scanner.pos = 0 - str.scanner.pos
+      return stub
     end
 
-    def rev_stub_at_least(min_chars, save_pos=true)
+    #   @param [Fixnum]
+    #   @param [true, false]
+    #   @return [String]
+    def rev_stub_at_least(min_chars, save_pos=false)
       str = self.reverse
       str.scanner.pos = 0 - scanner.pos if save_pos
 
-      return str.stub_at_least(min_chars, save_pos).reverse!
+      stub = str.stub_at_least(min_chars, save_pos).reverse!
+      scanner.pos = 0 - str.scanner.pos
+      return stub
     end
   end
 end
--- spec/string_stubber/core_ext_spec.rb
+++ spec/string_stubber/core_ext_spec.rb
@@ -1,2 +1,109 @@
 require File.expand_path("../spec_helper", File.dirname(__FILE__))
 
+describe StringStubber::CoreExt do
+  describe 'The CoreExt module should be mixed into String (via string_stubber.rb)' do
+    before :each do
+      @methods = [ :scanner, :stub_words, :stub_at_most, :stub_at_least ]
+    end
+
+    it 'should have class methods available' do
+      text = "it 'should have class methods available' <-- OMG META"
+
+      @methods.each do |method|
+        text.respond_to?(method).should be_true
+      end
+    end
+  end # Should be mixed into String
+
+  describe 'The stubbing methods should behave as documented' do
+    before :each do
+      @text   = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus vitae risus vitae lorem iaculis placerat. "
+      @count  = @text.split(/\W+/).count
+    end
+
+    it 'should return a StringScanner instance, when calling String#scanner' do
+      @text.scanner.should be_instance_of(StringScanner)
+    end
+
+    it 'should act upon the String instance, itself' do
+      @text.scanner.string.should be(@text)
+    end
+
+    describe 'Method: stub_words' do
+      it 'should retain its position' do
+        @text.stub_words(1)
+        @text.scanner.pos.should be(5)
+      end
+
+      it 'should always return a string' do
+        @text.stub_words( -100).should be_instance_of(String)
+        @text.stub_words(  -10).should be_instance_of(String)
+        @text.stub_words(    0).should be_instance_of(String)
+        @text.stub_words(   10).should be_instance_of(String)
+        @text.stub_words(  100).should be_instance_of(String)
+      end
+
+      it 'should correctly act on negative offsets' do
+        @text.stub_words(  -1).should       eql("placerat. ")
+        @text.stub_words( -10).should       eql("consectetuer adipiscing elit. Vivamus vitae risus vitae lorem iaculis placerat. ")
+        @text.stub_words(  -1, true).should eql("amet, consectetuer adipiscing elit. Vivamus vitae risus vitae lorem iaculis placerat. ")
+      end
+
+      it 'should never have trailing spaces'
+      it 'should return the expected number of words'
+    end
+
+    describe 'Method: stub_at_most' do
+      it 'should retain its position, relative to what was returned' do
+        @text.stub_at_most(3)
+        @text.scanner.pos.should be(0)
+
+        @text.stub_at_most(5)
+        @text.scanner.pos.should be(5)
+      end
+
+      it 'should always return a string' do
+        @text.stub_at_most( -100).should be_instance_of(String)
+        @text.stub_at_most(  -10).should be_instance_of(String)
+        @text.stub_at_most(    0).should be_instance_of(String)
+        @text.stub_at_most(   10).should be_instance_of(String)
+        @text.stub_at_most(  100).should be_instance_of(String)
+      end
+
+      it 'should correctly act on negative offsets' do
+      end
+      it 'should never have trailing spaces'
+    end
+
+    describe 'Method: stub_at_least' do
+      it 'should retain its position' do
+        @text.stub_at_least(3)
+        @text.scanner.pos.should be(5)
+
+        @text.stub_at_least(5)
+        @text.scanner.pos.should be(5)
+
+        @text.stub_at_least(6)
+        @text.scanner.pos.should be(11)
+
+        @text.stub_at_least(3)
+        @text.scanner.pos.should be(5)
+
+        @text.stub_at_least(4, true)
+        @text.scanner.pos.should be(11)
+      end
+
+      it 'should always return a string' do
+        @text.stub_at_least( -100).should be_instance_of(String)
+        @text.stub_at_least(  -10).should be_instance_of(String)
+        @text.stub_at_least(    0).should be_instance_of(String)
+        @text.stub_at_least(   10).should be_instance_of(String)
+        @text.stub_at_least(  100).should be_instance_of(String)
+      end
+
+      it 'should correctly act on negative offsets' do
+      end
+      it 'should never have trailing spaces'
+    end
+  end # Docs check
+end
